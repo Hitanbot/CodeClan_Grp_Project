dc0b288730b14ed443f6990e8f3a8ccbd6c9407c
Stuart Meldrum <MrMeldrum@live.co.uk>
2020-06-26 14:54:27 +0100

    Dice.vue created

---
+++ client/src/components/player_info.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div>
+    <p v-for="player in players" :player="player">{{player.name}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'player_list',
+  props: ['players']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
