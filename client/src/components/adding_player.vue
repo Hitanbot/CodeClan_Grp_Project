1e745ffd5f87b0c072a9561a27e16bc56cf530b7
Stuart Meldrum <MrMeldrum@live.co.uk>
2020-06-27 12:03:33 +0100

    added player_info.vue

---
+++ client/src/components/adding_player.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <form @submit="handleFormSubmit">
+    <h2>Add a Player</h2>
+    <div class="formWrap">
+      <label for="playername">Player Name: </label>
+      <input type="text" id="playername" v-model="name" />
+    </div>
+
+    <input type="submit" value="Add Player" id="saveplayer">
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'added_player',
+  data() {
+    return {
+      'name': "",
+      'playerPosition': 0,
+      'isTurn': false
+    }
+  },
+  methods: {
+    handleFormSubmit(event) {
+      event.preventDefault();
+      const newPlayer = {
+        name: this.name,
+        playerPosition: this.playerPosition,
+        isTurn: this.isTurn
+      }
+    }
+  }
+
+}
+</script>
+
+<style lang="css" scoped>
+</style>
