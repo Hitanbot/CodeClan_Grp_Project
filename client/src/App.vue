11e106ba072e6b91dd790cf4ed184e142205a830
Tino <tinodafunk@googlemail.com>
2020-06-28 14:26:25 +0100

    basic_app_layout

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-  </div>
+  <!-- <div> -->
+<h2 id="title">Snakes and Ladders</h2>
+<!-- </div> -->
+
+    <img src="./assets/game_board.jpg" id="board">
+    <img src="./assets/dice.jpg" id="dice">
+    <img src="./assets/players.jpg" id="players">
+
+</div>
 </template>
 
 <script>
@@ -22,6 +29,54 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-image: linear-gradient(to right, lime , green);
+  /* margin-top: 60px; */
 }
+
+#title {
+    background-image: linear-gradient(to right, lime , green);
+}
+
+
+#board {
+  position: absolute;
+  max-width: 40%;
+  height: auto;
+  align-self: flex-end;
+  flex-flow: column-reverse;
+}
+
+#players {
+  position: absolute;
+  max-width: 20%;
+  height: auto;
+  left: 136px;
+  top: 90px;
+}
+
+
+
+#dice{
+  position: absolute;
+  max-width: 10%;
+  height: auto;
+  left: 200px;
+  bottom: 20px;
+}
+
+
+{
+
+}
+
+.square{
+
+}
+
+.player{
+
+}
+
+
+
 </style>
